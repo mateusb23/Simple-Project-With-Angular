fc6e3a4a9429071570665e7aa0e4c0e8adcda225
mateusb23 <mateusb.bispo@gmail.com>
2023-06-30 23:54:06 -0300

    parent-data component creation

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent {
+  name: string = 'Mateus';
+  age: number = 30;
+  job = 'Software Developer';
+  hobbies = ['Correr', 'Ler Literatura de horror/terror', 'Andar de bike com a minha namorada'];
+  car = {
+    name: "Punto",
+    year: 2019
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
