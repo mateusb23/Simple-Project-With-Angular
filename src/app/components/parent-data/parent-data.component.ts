aab1a5c223246e17343ca8180100a6c9e68b40fa
mateusb23 <mateusb.bispo@gmail.com>
2023-07-03 20:43:06 -0300

    project update

--- src/app/components/parent-data/parent-data.component.ts
+++ src/app/components/parent-data/parent-data.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-parent-data',
@@ -6,8 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./parent-data.component.css']
 })
 export class ParentDataComponent {
+  @Input() name: string = ''
+
   username: string = 'Fernanda';
   title: string = 'title'
 
   constructor() { }
+
+  ngOnInit(): void{}
 }
