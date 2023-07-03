36d5f57af48eabaf3a722b626a751eb3a55df9ca
mateusb23 <mateusb.bispo@gmail.com>
2023-07-02 21:32:21 -0300

    parent-data component updated

--- src/app/components/parent-data/parent-data.component.ts
+++ src/app/components/parent-data/parent-data.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./parent-data.component.css']
 })
 export class ParentDataComponent {
+  username: string = 'Fernanda';
+  title: string = 'title'
 
+  constructor() { }
 }
